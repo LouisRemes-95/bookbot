1719dc195ed39d68da79b6f891cf39f1e37fd06d
Louis Remes <louisremes@hotmail.be>
2025-05-14 20:58:56 +0200

    add word count

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
+    
+def count_words(text):
+    return len(text.split())
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    print(f"{count_words(text)} words found in the document")
+
+main()

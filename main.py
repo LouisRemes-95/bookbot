95399c767330be582a230d3de1c21fff9bece2c4
Louis Remes <louisremes@hotmail.be>
2025-05-14 21:03:45 +0200

    refactor

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
+from stats import get_num_words
+
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
-    
-def count_words(text):
-    return len(text.split())
 
 def main():
     text = get_book_text("books/frankenstein.txt")
-    print(f"{count_words(text)} words found in the document")
+    print(f"{get_num_words(text)} words found in the document")
 
 main()

0e592d8cd7e4893fdf887798e243dfe70dcd0232
Louis Remes <louisremes@hotmail.be>
2025-05-15 21:24:56 +0200

    End of project

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from stats import get_num_words, get_num_char, sort_char
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f"Found {get_num_words(text)} total words")
-    print("--------- Character Count -------")
+    else:
+        text = get_book_text(sys.argv[1])
 
-    for char in sort_char(get_num_char(text)):
-        if char["char"].isalpha():
-            print(f"{char['char']}: {char['num']}")
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}")
+        print("----------- Word Count ----------")
+        print(f"Found {get_num_words(text)} total words")
+        print("--------- Character Count -------")
 
-    print("============= END ===============")
+        for char in sort_char(get_num_char(text)):
+            if char["char"].isalpha():
+                print(f"{char['char']}: {char['num']}")
+
+        print("============= END ===============")
 
 main()
